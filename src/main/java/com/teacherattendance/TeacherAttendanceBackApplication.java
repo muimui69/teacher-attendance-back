252bef424d152376a737cf0b3fc0a7cde3a5c94f
jdguior <137825829+jdguior@users.noreply.github.com>
2024-06-16 03:00:11 -0400

    user_act

--- src/main/java/com/teacherattendance/TeacherAttendanceBackApplication.java
+++ src/main/java/com/teacherattendance/TeacherAttendanceBackApplication.java
@@ -5,12 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import com.teacherattendance.config.WebConfig;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 @SpringBootApplication
 @Import(WebConfig.class)
 public class TeacherAttendanceBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeacherAttendanceBackApplication.class, args);
+	}
+
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI().
+		info( new Info()
+				.title("SISTEMA DE REGISTRO DE ASISTENCIA DE DOCENTES")
+				.version("0.0.0")
+				.description("Aplicacion simple de spring boot 3 con swagger")
+				.termsOfService("http://swagger.io/terms/")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+
+		);
 	}
 
 }
--- src/main/java/com/teacherattendance/auth/AuthResponse.java
+++ src/main/java/com/teacherattendance/auth/AuthResponse.java
@@ -1,5 +1,7 @@
 package com.teacherattendance.auth;
 
+import com.teacherattendance.entity.Roles;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,5 +14,10 @@ import lombok.NoArgsConstructor;
 public class AuthResponse {
 	
 	String token;
+	String email;
+	Roles role;
+	String nombre;
+	String apellido;
+	Long id;
 	
 }
